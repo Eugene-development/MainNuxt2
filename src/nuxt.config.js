3729d89405e605bbc66d00cc97c2188f63a24a30
indexpro24@gmail.com <indexpro24@gmail.com>
2021-12-27 22:29:54 +0300

    git init

--- src/nuxt.config.js
+++ src/nuxt.config.js
@@ -2,27 +2,27 @@
 export default {
   mode: 'universal',
   server: {
-    port: 1111,
+    port: 4160,
     host: '0.0.0.0'
   },
   /*
   ** Headers of the page
   */
   head: {
-    title: process.env.npm_package_name || '',
+    title:  'Гост Ремонт',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { hid: 'description', name: 'description', content:  'Описание' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/Logo/favicon.ico' }
     ]
   },
   /*
   ** Customize the progress-bar color
   */
-  loading: { color: '#fff' },
+  loading: { color: 'red' },
   /*
   ** Global CSS
   */
